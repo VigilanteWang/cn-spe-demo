3584360a28bd57d16eec6d150bae5f396049e10b
VigilanteWang <carbinewang@gmail.com>
2025-05-16 13:12:39 +0800

    Add: SPE basic framework

---
+++ src/common/constants.ts
@@ -0,0 +1,6 @@
+export const CLIENT_ENTRA_APP_CLIENT_ID = 'a036da11-eb22-4436-99fb-94efa1861999';
+export const CLIENT_ENTRA_APP_AUTHORITY = '';
+
+export const API_SERVER_URL = 'http://localhost:3001';
+
+export const CONTAINER_TYPE_ID = '';
